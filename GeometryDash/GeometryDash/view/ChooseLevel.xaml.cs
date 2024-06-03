935a4b2a0c2176ebf237d27523ac836cc0cd9056
mathis-de-sousa-pro <mathis.dsa39@gmail.com>
2024-06-03 10:50:24 +0200

    creation de l'ihm

---
+++ GeometryDash/GeometryDash/view/ChooseLevel.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GeometryDash
+{
+    public partial class ChooseLevel : Window
+    {
+        private ObservableCollection<Level> _levels;
+        private ICollectionView _levelsView;
+
+        public ChooseLevel()
+        {
+            InitializeComponent();
+
+            // Créez vos niveaux ici
+            _levels = new ObservableCollection<Level>();
+            for (int i = 1; i <= 20; i++)
+            {
+                _levels.Add(new Level
+                {
+                    Color = new SolidColorBrush(Color.FromRgb((byte)(i * 10), (byte)(i * 5), (byte)(i * 20)))
+                });
+            }
+
+            _levelsView = CollectionViewSource.GetDefaultView(_levels);
+            LevelsControl.Content = _levelsView.CurrentItem;        }
+
+        private void PreviousLevel(object sender, RoutedEventArgs e)
+        {
+            if (_levelsView.CurrentPosition > 0)
+            {
+                _levelsView.MoveCurrentToPrevious();
+                LevelsControl.Content = _levelsView.CurrentItem;
+            }
+        }
+
+        private void NextLevel(object sender, RoutedEventArgs e)
+        {
+            if (_levelsView.CurrentPosition < _levels.Count - 1)
+            {
+                _levelsView.MoveCurrentToNext();
+                LevelsControl.Content = _levelsView.CurrentItem;
+            }
+        }
+
+        private void Level_Click(object sender, RoutedEventArgs e)
+        {
+            // Ajoutez ici le code pour gérer le clic sur un niveau
+        }
+    }
+
+    public class Level
+    {
+        public SolidColorBrush Color { get; set; }
+    }
+}
--- GeometryDash/GeometryDash/view/MainWindow.xaml.cs
+++ GeometryDash/GeometryDash/view/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using IUTGame;
 using IUTGame.WPF;
 
-namespace GeometryDash
+namespace GeometryDash.view
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -29,10 +18,7 @@ namespace GeometryDash
             IScreen screen = new WPFScreen(Canvas);
             _game = new GeometryDashGame(screen);
         }
-        private void Play(object sender, RoutedEventArgs e)
-        {
-            _game.Run();
-        }
+        
         private void SliderSound_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             _game.BackgroundVolume = e.NewValue/100.0;
@@ -41,6 +27,18 @@ namespace GeometryDash
         private void SliderWind_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             _game.Wind2 = e.NewValue;
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            _game.Run();
+        }
+
+        private void ChooseLevelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Open the ChooseLevel window
+            ChooseLevel chooseLevelWindow = new ChooseLevel();
+            chooseLevelWindow.Show();
         }
     }
 }
